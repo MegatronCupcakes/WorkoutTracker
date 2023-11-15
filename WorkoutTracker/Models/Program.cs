2cc419944ff0d6f84908716f3df916e9f5f82b75
Dallas Howard <dallas@galacticwidgets.com>
2023-11-15 13:40:34 -0500

    added defaults

--- WorkoutTracker/Models/Program.cs
+++ WorkoutTracker/Models/Program.cs
@@ -1,6 +1,4 @@
-﻿using WorkoutTracker.Models;
-
-namespace WorkoutTracker.Types
+﻿namespace WorkoutTracker.Models
 {
     /// <summary>
     /// 
@@ -10,9 +8,9 @@ namespace WorkoutTracker.Types
     /// A Program is the top level; it's a series of Routines (and Routines are a series of Exercises with their associated properties)
     /// </summary>
     public class Program : ActivityBase
-    {              
-        public bool Active { get; set; }                   
-        public string[] Routines { get; set; }
+    {
+        public bool Active { get; set; } = false;                   
+        public string[] Routines { get; set; } = new string[0];
         public Program() { }
         public Program(string id, bool active, string name, string[] routines)
         {
