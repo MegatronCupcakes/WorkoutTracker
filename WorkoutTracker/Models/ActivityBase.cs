f9aeb70a4c6d30a97f0f529189e90d93e0b48590
Dallas Howard <dallas@galacticwidgets.com>
2023-11-15 13:41:25 -0500

    added default value for _id; added support for CreatedAt and UpdatedAt timestamps

--- WorkoutTracker/Models/ActivityBase.cs
+++ WorkoutTracker/Models/ActivityBase.cs
@@ -2,8 +2,10 @@
 {
     public class ActivityBase
     {
-        public string _id { get; set; }
+        public string _id { get; set; } = Guid.NewGuid().ToString();
         public string Name { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
         public ActivityBase() { }
     }
 }
