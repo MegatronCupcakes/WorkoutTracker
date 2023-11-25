d8a033f411f982b9b99dfe05049de7e6b57d539a
Dallas Howard <dallas@galacticwidgets.com>
2023-11-25 15:58:48 -0500

    initial chart.js support for metrics

--- WorkoutTracker/Program.cs
+++ WorkoutTracker/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using pax.BlazorChartJs;
 using WorkoutTracker;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddChartJs(options =>
+{
+    // default
+    options.ChartJsLocation = "https://cdn.jsdelivr.net/npm/chart.js";
+    options.ChartJsPluginDatalabelsLocation = "https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2";
+});
 await builder.Build().RunAsync();
