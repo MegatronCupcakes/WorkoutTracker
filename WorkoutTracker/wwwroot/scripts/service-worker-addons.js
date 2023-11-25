bd4e3d1b031034723e377b16ac19ac207452d857
Dallas Howard <dallas@galacticwidgets.com>
2023-11-25 15:32:57 -0500

    initial implementation for notification support

---
+++ WorkoutTracker/wwwroot/scripts/service-worker-addons.js
@@ -0,0 +1,16 @@
+self.addEventListener('push', event => {
+    const payload = event.data.json();
+    event.waitUntil(
+        self.registration.showNotification('WorkoutTracker', {
+            body: payload.message,
+            icon: 'img/icon-512.png',
+            vibrate: [100, 50, 100],
+            data: { url: payload.url }
+        })
+    );
+});
+
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    event.waitUntil(clients.openWindow(event.notification.data.url));
+});
