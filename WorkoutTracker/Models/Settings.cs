48a207c996915bfb20f6e46de226c1a1fcdf7388
Dallas Howard <dallas@galacticwidgets.com>
2023-11-30 16:57:23 -0500

    added wizard on startup

--- WorkoutTracker/Models/Settings.cs
+++ WorkoutTracker/Models/Settings.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using WorkoutTracker.DataAccess;
 
 namespace WorkoutTracker.Models
 {
@@ -8,9 +9,20 @@ namespace WorkoutTracker.Models
         public string _id { get; set; } = Guid.NewGuid().ToString();
         [Required]
         public bool NotificationsEnabled { get; set; } = false;
+        public bool WizardEnabled { get; set; } = true;
         public Settings()
         {
             
         }
+        public async static Task<Settings> GetSettings(Dictionary<string, IndexedDb> collections)
+        {
+            var appSettings = await collections["settings"].FindOne<WorkoutTracker.Models.Settings>(@"{}");
+            if (appSettings == null)
+            {
+                appSettings = new WorkoutTracker.Models.Settings();
+                var inserted = await collections["settings"].Insert(appSettings);
+            }
+            return appSettings;
+        }        
     }
 }
