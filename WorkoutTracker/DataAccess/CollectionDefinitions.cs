74f9aa7bee30e0a734f3205d037047066e0b3f2d
Dallas Howard <dallas@galacticwidgets.com>
2023-11-30 11:38:49 -0500

    moved collection definitions to static class

---
+++ WorkoutTracker/DataAccess/CollectionDefinitions.cs
@@ -0,0 +1,15 @@
+﻿namespace WorkoutTracker.DataAccess
+{
+    public static class CollectionDefinitions
+    {
+        public static Dictionary<string, List<string>> Definitions = new Dictionary<string, List<string>>()
+        {
+            {"workouts", new List<string>(){ "startedAt", "completedAt" }},
+            {"programs", new List<string>(){ "createdAt" } },
+            {"routines", new List<string>(){ "createdAt" }},
+            {"exercises", new List<string>(){ "createdAt" }},
+            {"settings", new List<string>(){ "createdAt" }}
+        };
+    }
+    
+}
